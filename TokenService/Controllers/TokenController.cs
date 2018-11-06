f3cb7f160cea93f8bb98a3f25f36e62aa2c0eb51
Adrian <sci.adrian@gmail.com>
2018-11-06 13:49:03 +0200

    Created JWT Generator

---
+++ TokenService/Controllers/TokenController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Web.Http;
+using UserModel = Shared.Identity_Provider.User;
+
+namespace TokenService.Controllers
+{
+    public class TokenController : ApiController
+    {
+        [Route("api/authenticate")]
+        [HttpPost]
+        [AllowAnonymous]
+        public string Get(UserModel user)
+        {
+            if (CheckUser(user))
+            {
+                return JwtManager.GenerateToken(user);
+            }
+
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+
+        public bool CheckUser(UserModel user)
+        {
+            // should check in the database
+            return true;
+        }
+    }
+}
