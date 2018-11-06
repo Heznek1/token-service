de56a8e223d24a76ca5e673852a8b63d4bf44f14
Adrian <sci.adrian@gmail.com>
2018-11-06 09:54:25 +0200

    Short updates

--- TokenService/Global.asax.cs
+++ TokenService/Global.asax.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
-using Autofac;
-using Autofac.Integration.WebApi;
+﻿using System.Web.Http;
 
 namespace TokenService
 {
